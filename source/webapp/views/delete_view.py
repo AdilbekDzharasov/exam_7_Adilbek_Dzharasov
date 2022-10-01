239d996204c4256e676a949ec56833b81f298705
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-01 13:58:12 +0600

    Добавлен метод для удаления записи

--- source/webapp/views/delete_view.py
+++ source/webapp/views/delete_view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Entry
 
 
-def delete_entry(request, pk):
+def delete_entries(request, pk):
     entry = get_object_or_404(Entry, pk=pk)
     if request.method == 'GET':
         return render(request, 'delete.html', context={'entry': entry})
